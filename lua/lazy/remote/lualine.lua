e8268ebf7d7cff84af30dac1ff0b870eca46deaa
João Bezerra <joaobzzr@gmail.com>
2025-08-05 12:04:21 -0300

    Update

--- lua/lazy/remote/lualine.lua
+++ lua/lazy/remote/lualine.lua
@@ -1,10 +1,45 @@
+local function extract_hl(name)
+  local hl = vim.api.nvim_get_hl(0, { name = name })
+  return {
+    fg = hl.fg and string.format('#%06x', hl.fg) or nil,
+    bg = hl.bg and string.format('#%06x', hl.bg) or nil,
+    gui = hl.bold and 'bold' or nil,
+  }
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    'ramojus/mellifluous.nvim',
   },
-  config = function()
-    require('lualine').setup {}
+  opts = function()
+    return {
+      options = {
+        theme = {
+          normal = {
+            a = extract_hl 'LuaLineA_ModeNormal',
+            b = extract_hl 'LuaLineB_ModeNormal',
+            c = extract_hl 'LuaLineC_ModeNormal',
+          },
+          insert = {
+            a = extract_hl 'LuaLineA_ModeInsert',
+          },
+          visual = {
+            a = extract_hl 'LuaLineA_ModeVisual',
+          },
+          replace = {
+            a = extract_hl 'LuaLineA_ModeReplace',
+          },
+          inactive = {
+            a = extract_hl 'LuaLineA_Inactive',
+            b = extract_hl 'LuaLineB_Inactive',
+            c = extract_hl 'LuaLineC_Inactive',
+          },
+        },
+      },
+      sections = {
+        lualine_x = { 'filetype' },
+      },
+    }
   end,
 }
--- lua/lazy/remote/mellifluous.lua
+++ lua/lazy/remote/mellifluous.lua
@@ -33,7 +33,39 @@ return {
             hl.set('CursorLine', { bg = colors.bg })
             hl.set('NormalFloat', { fg = colors.fg4, bg = colors.bg })
             hl.set('FloatBorder', { fg = colors.fg4, bg = colors.bg })
-            hl.set('StatusLine', { fg = '#ff0000', bg = '#00ff00' })
+
+            -- status line
+            local sl_fg = colors.fg2.hex
+            local sl_fg_red = colors.red:saturated(20).hex
+            local sl_fg_green = colors.green:saturated(20).hex
+            local sl_fg_yellow = colors.yellow:saturated(20).hex
+            local sl_fg_purple = colors.purple:saturated(20).hex
+            local sl_fg_blue = colors.blue:saturated(20).hex
+
+            local sl_bg = colors.bg2.hex
+            local sl_bg_gray = colors.bg3:lightened(5).hex
+            local sl_bg_red = colors.red:darkened(30):saturated(20).hex
+            local sl_bg_blue = colors.blue:darkened(40):saturated(20).hex
+            local sl_bg_green = colors.green:darkened(40):saturated(20).hex
+            local sl_bg_purple = colors.purple:darkened(30):saturated(20).hex
+
+            hl.set('LuaLineA_ModeNormal', { fg = sl_fg, bg = sl_bg_blue, bold = true })
+            hl.set('LuaLineB_ModeNormal', { fg = sl_fg, bg = sl_bg_gray })
+            hl.set('LuaLineC_ModeNormal', { fg = sl_fg, bg = colors.bg2.hex })
+            hl.set('LuaLineA_ModeInsert', { fg = sl_fg, bg = sl_bg_green, bold = true })
+            hl.set('LuaLineA_ModeVisual', { fg = sl_fg, bg = sl_bg_purple, bold = true })
+            hl.set('LuaLineA_ModeReplace', { fg = sl_fg, bg = sl_bg_red, bold = true })
+            hl.set('LuaLineA_Inactive', { fg = sl_fg, bg = sl_bg, bold = true })
+            hl.set('LuaLineB_Inactive', { fg = sl_fg, bg = sl_bg })
+            hl.set('LuaLineC_Inactive', { fg = sl_fg, bg = sl_bg })
+
+            hl.set('DiagnosticError', { fg = sl_fg_red })
+            hl.set('DiagnosticWarn', { fg = sl_fg_yellow })
+            hl.set('DiagnosticInfo', { fg = sl_fg_purple })
+            hl.set('DiagnosticHint', { fg = sl_fg_blue })
+            hl.set('LuaLineDiffAdd', { fg = sl_fg_green })
+            hl.set('LuaLineDiffChange', { fg = sl_fg_yellow })
+            hl.set('LuaLineDiffDelete', { fg = sl_fg_red })
           end,
         },
       },
