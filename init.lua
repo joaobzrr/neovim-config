7f356a548722f364de458c132e40247f8448e517
João Bezerra <joaobzzr@gmail.com>
2025-11-16 15:06:54 -0300

    Update

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
--- OPTIONS 
+-- OPTIONS
 
 -- Leader keys
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Basics
-vim.opt.number = true -- Absolute line numbers
+vim.opt.number = true         -- Absolute line numbers
 vim.opt.relativenumber = true -- Relative numbers for motion
-vim.opt.cursorline = true -- Highlight current line
-vim.opt.signcolumn = 'yes' -- Prevent text shifting when signs appear
-vim.opt.scrolloff = 10 -- Keep 10 lines of padding when scrolling
-vim.opt.mouse = 'a' -- Enable mouse in all modes
-vim.opt.showmode = false -- Mode shown by statusline instead
+vim.opt.cursorline = true     -- Highlight current line
+vim.opt.signcolumn = 'yes'    -- Prevent text shifting when signs appear
+vim.opt.scrolloff = 10        -- Keep 10 lines of padding when scrolling
+vim.opt.mouse = 'a'           -- Enable mouse in all modes
+vim.opt.showmode = false      -- Mode shown by statusline instead
 vim.opt.winborder = 'rounded' -- Rounded floating window borders
 
 -- Indentation
@@ -29,7 +29,7 @@ vim.opt.smartcase = true -- Case-sensitive only when uppercase used
 -- Whitespace display
 vim.opt.list = true
 vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
-vim.opt.fillchars = vim.opt.fillchars + 'diff:╱'  -- Character used in diff filler lines
+vim.opt.fillchars = vim.opt.fillchars + 'diff:╱' -- Character used in diff filler lines
 
 -- Window behavior
 vim.opt.splitright = true
@@ -48,12 +48,12 @@ vim.opt.timeoutlen = 300
 vim.opt.guifont = 'JetBrainsMonoNL NFM:h10'
 
 
--- GENERAL KEYMAPS 
+-- GENERAL KEYMAPS
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>') -- Save and reload config
 
 -- Basic editing
-vim.keymap.set('n', '<leader>w', ':w<CR>') -- Save file
+vim.keymap.set('n', '<leader>w', ':w<CR>')          -- Save file
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- Clear search highlight
 
 -- Quickfix navigation
@@ -103,7 +103,7 @@ vim.keymap.set('n', '<leader>qf', function()
 end)
 
 
--- AUTOCOMMANDS 
+-- AUTOCOMMANDS
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -124,13 +124,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.cmd('set completeopt+=noselect')
 
 
--- LSP 
+-- LSP
 
-vim.lsp.enable({ 'lua_ls' }) -- Enable LSP servers
+vim.lsp.enable({ 'lua_ls' })                          -- Enable LSP servers
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format) -- Format buffer
 
 
--- PACKAGES 
+-- PACKAGES
 
 vim.pack.add({
   "https://github.com/mason-org/mason.nvim",
@@ -187,12 +187,34 @@ vim.keymap.set('n', '-', '<CMD>Oil<CR>')
 vim.keymap.set('n', '<leader>-', require('oil').toggle_float)
 
 -- Diffview
-require('diffview').setup({ enhanced_diff_hl = true })
+require('diffview').setup({
+  use_icons = false,
+  enhanced_diff_hl = true,
+  hooks = {
+    diff_buf_win_enter = function(_, _, ctx)
+      if ctx.layout_name:match '^diff2' then
+        if ctx.symbol == 'a' then
+          vim.opt_local.winhl = table.concat({
+            'DiffAdd:DiffviewDiffDelete',
+            'DiffChange:DiffviewDiffDelete',
+            'DiffText:DiffviewDiffTextRed',
+          }, ',')
+        elseif ctx.symbol == 'b' then
+          vim.opt_local.winhl = table.concat({
+            'DiffAdd:DiffviewDiffAdd',
+            'DiffChange:DiffviewDiffAdd',
+            'DiffText:DiffviewDiffTextGreen',
+          }, ',')
+        end
+      end
+    end,
+  }
+})
 vim.keymap.set('n', '<leader>dv', ':DiffviewOpen<CR>')
 vim.keymap.set('n', '<leader>dc', ':DiffviewClose<CR>')
 
 
---  COLORS 
+--  COLORS
 
 require('mellifluous').setup({
   mellifluous = {
@@ -201,18 +223,15 @@ require('mellifluous').setup({
       dark = {
         colors = function(_)
           local color = require 'mellifluous.color'
-          local green = color.new_from_hsl { h = 130, s = 30, l = 70 } -- Custom warm green tint
-          return {
-            strings = green,
-            green = green,
-          }
+          local green = color.new_from_hsl { h = 130, s = 30, l = 70 }
+          return { strings = green, green = green }
         end,
       },
     },
     highlight_overrides = {
       dark = function(hl, colors)
-        hl.set('MiniPickNormal', { bg = colors.bg }) -- Match background with theme
-        hl.set('MiniPickBorder', { fg = colors.bg4 }) -- Subtle border
+        hl.set('MiniPickNormal', { bg = colors.bg })
+        hl.set('MiniPickBorder', { fg = colors.bg4 })
       end,
     },
   },
@@ -223,6 +242,13 @@ require('mellifluous').setup({
 
 vim.cmd.colorscheme('mellifluous')
 
-vim.cmd(':hi statusline guibg=NONE')
+vim.api.nvim_set_hl(0, 'statusline', { bg = nil })
+
+vim.api.nvim_set_hl(0, 'DiffDelete', { fg = '#2b2b2b' })
+vim.api.nvim_set_hl(0, 'DiffviewDiffAdd', { bg = '#152112' })
+vim.api.nvim_set_hl(0, 'DiffviewDiffTextGreen', { bg = '#2a4125' })
+vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = '#24150f' })
+vim.api.nvim_set_hl(0, 'DiffviewDiffTextRed', { bg = '#492c1d' })
+
 
 -- vim: ts=2 sts=2 sw=2 et
