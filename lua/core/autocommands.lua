21eaaa8d9f0a10138b07a202782736a478ce1a65
João Bezerra <joaobzzr@gmail.com>
2025-11-19 00:31:03 -0300

    Update

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,5 +1,5 @@
 -- Highlight yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function() vim.highlight.on_yank() end,
 })
